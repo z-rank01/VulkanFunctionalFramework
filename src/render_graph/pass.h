2f1d289c39491cb6351402a9ceae03011820703e
陈皓东 <chenhd0110@qq.com>
2025-12-09 23:38:33 +0800

    [feature] 添加 pass 的 topology 相关的结构体

--- src/render_graph/pass.h
+++ src/render_graph/pass.h
@@ -2,13 +2,32 @@
 
 #include <vulkan/vulkan.hpp>
 
-namespace render_graph 
+namespace render_graph
 {
+    using pass_handle    = uint32_t;
+    using resouce_handle = uint32_t;
+
     // graph topology
-    struct graph_topology
+
+    // one dimesion array to represent the read resource of each pass
+    struct read_dependency
     {
-        
+        std::vector<resouce_handle> read_list;
+        std::vector<pass_handle>    offsets;
+        std::vector<uint32_t>       lengthes;
     };
 
+    // one dimesion array to represent the write resource of each pass
+    struct write_dependency
+    {
+        std::vector<resouce_handle> write_list;
+        std::vector<pass_handle>    offsets;
+        std::vector<uint32_t>       lengthes;
+    };
 
-};
+    struct graph_topology
+    {
+        std::vector<pass_handle> passes;
+    };
+
+}; // namespace render_graph
