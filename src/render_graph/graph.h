7318b14c0d40a626b1ca70b2d21d7497b46724e1
陈皓东 <chenhaodong@galasports.com>
2025-12-19 18:29:59 +0800

    [fix] 更正拼写 resouce 为 resource

--- src/render_graph/graph.h
+++ src/render_graph/graph.h
@@ -5,24 +5,24 @@
 namespace render_graph
 {
     using pass_handle    = uint32_t;
-    using resouce_handle = uint32_t;
+    using resource_handle = uint32_t;
 
     // graph topology
 
     // one dimesion array to represent the read resource of each pass
     struct read_dependency
     {
-        std::vector<resouce_handle> read_list;
-        std::vector<pass_handle>    begins;
-        std::vector<pass_handle>    lengthes;
+        std::vector<resource_handle> read_list;
+        std::vector<resource_handle> begins;
+        std::vector<resource_handle> lengthes;
     };
 
     // one dimesion array to represent the write resource of each pass
     struct write_dependency
     {
-        std::vector<resouce_handle> write_list;
-        std::vector<pass_handle>    begins;
-        std::vector<pass_handle>    lengthes;
+        std::vector<resource_handle> write_list;
+        std::vector<resource_handle> begins;
+        std::vector<resource_handle> lengthes;
     };
 
     struct graph_topology
